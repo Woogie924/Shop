4472db764a7e38ebc261a6f23c4f05968249771b
Woogie924 <hsw924@gmail.com>
2022-05-15 23:30:58 +0800

    [#1] 엔티티 설계 (추후에 멤버변수 네이밍 필요)

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -5,26 +5,40 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name="item")
 @Getter
 @Setter
 @ToString
 public class Item {
 
-    private Long id;
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;        // 상품 코드
 
-    private String itemNm;
+    @Column(nullable = false,
+            length = 50)
+    private String itemNm;  // 상품명
+    
+    @Column(name="price",
+            nullable = false)
+    private int price;  // 가격
 
-    private int price;
+    @Column(nullable = false)
+    private int stockQuantity;    // 재고 수량
 
-    private int stockNumber;
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;  // 상품 상세 설명
 
-    private String itemDetail;
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;  // 상품 판매 상태
+    
+    private LocalDateTime localDateTime;    // 등록 시간
 
-    private ItemSellStatus itemSellStatus;
-
-    private LocalDateTime localDateTime;
-
-    private LocalDateTime updateTime;
+    private LocalDateTime updateTime;       // 수정 시간
 }
