1082e64ed09d741f5257ce72b93b5d4d0d14f138
Woogie924 <hsw924@gmail.com>
2022-07-02 17:12:48 +0800

    [#1]  thymeleaf 이용해서 간단한 웹 레이아웃 작성

--- src/main/java/com/shop/ThymeleafExampleController.java
+++ src/main/java/com/shop/ThymeleafExampleController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/thymeleaf")
@@ -32,6 +34,40 @@ public class ThymeleafExampleController {
 
         model.addAttribute("item", itemDto);
         return "thymeleafExample/ex02";
+    }
+
+    @GetMapping("/ex03")
+    public String thymeleafExample03(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for(int i=1; i<=10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setItemName("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setCreatedAt(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafExample/ex03";
+    }
+
+    @GetMapping("/ex04")
+    public String thymeleafExample04() {
+        return "thymeleafExample/ex04";
+    }
+
+    @GetMapping("/ex05")
+    public String thymeleafExample05(String param1, String param2, Model model) {
+        model.addAttribute("param1",param1);
+        model.addAttribute("param2",param2);
+        return "thymeleafExample/ex05";
+    }
+
+    @GetMapping("/ex06")
+    public String thymeleafExample06() {
+        return "thymeleafExample/ex06";
     }
 
 }
